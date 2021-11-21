40f2567aadb1930f8f39e69f9a299cd5de8a35c5
Leandro Camilotti <lecamilotti@gmail.com>
2021-11-21 09:09:23 +0100

    commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function typeWriter(elementID, text) {
+  let i = 0;
+  function write() {
+    var speed = 20;
+    if (i < text.length) {
+      const a = (document.getElementById(elementID).innerHTML +=
+        text.charAt(i));
+      i++;
+      setTimeout(write, speed);
+    }
+  }
+  write();
+}
+
+document.querySelectorAll("ul#menu-content li a").forEach((item) => {
+  item.addEventListener("click", () => {
+    document.querySelector("input[type=checkbox").checked = false;
+  });
+});
+typeWriter("type-name", "Web Developer");
